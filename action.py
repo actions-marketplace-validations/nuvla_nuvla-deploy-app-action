95868aa5468c2272d0141dae88f88604a09ee00b
Cristovao Cordeiro <cris@sixsq.com>
2021-08-12 10:50:03 +0200

    check depl state

--- action.py
+++ action.py
@@ -3,7 +3,31 @@
 
 import argparse
 import atexit
+import signal
 from nuvla.api import Api
+from contextlib import contextmanager
+
+
+@contextmanager
+def timeout(deadline, err):
+    # Register a function to raise a TimeoutError on the signal.
+    signal.signal(signal.SIGALRM, raise_timeout)
+    # Schedule the signal to be sent after ``time``.
+    signal.alarm(deadline)
+
+    try:
+        yield
+    except TimeoutError:
+        raise Exception(err)
+    finally:
+        # Unregister the signal so it won't be triggered
+        # if the timeout is not reached.
+        signal.signal(signal.SIGALRM, signal.SIG_IGN)
+
+
+def raise_timeout(signum, frame):
+    raise TimeoutError
+
 
 def init():
     """ Parse command-line args
@@ -43,6 +67,14 @@ def init():
     return parser.parse_args()
 
 
+def stop_deployment(api, did):
+    api.get(did + "/stop")
+
+
+def delete_deployment(api, did):
+    api.delete(did)
+
+
 if __name__ == '__main__':
     args = init()
 
@@ -53,7 +85,7 @@ if __name__ == '__main__':
     depl = api.add('deployment', {'module': {'href': args.module_id}}).data
     depl_id = depl.get('resource-id')
 
-    atexit.register(api.delete, depl_id)
+    atexit.register(delete_deployment, api, depl_id)
 
     depl = api.get(depl_id).data
     depl['parent'] = args.credential_id
@@ -69,7 +101,19 @@ if __name__ == '__main__':
 
     api.get(depl_id + "/start")
 
-    atexit.unregister(api.delete)
+    atexit.register(stop_deployment, api, depl_id)
+
+    with timeout(600, f'Timed out while waiting for deployment {depl_id} to get STARTED'):
+        while True:
+            live_depl = api.get(depl_id).data
+            state = live_depl.get('state')
+            if state == 'ERROR':
+                raise Exception(f'Deployment {depl_id} failed to start')
+            elif state == 'STARTED':
+                break
+
+    atexit.unregister(stop_deployment)
+    atexit.unregister(delete_deployment)
 
     print(f"::set-output name=DEPLOYMENT_ID::{depl_id}")
 
